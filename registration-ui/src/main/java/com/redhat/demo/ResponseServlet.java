9dab1db690b62cf8fbe8d7300d3a47321b2d6bd2
developer <gnunn@redhat.com>
2020-04-23 20:12:37 -0700

    Add new registration ui

---
+++ registration-ui/src/main/java/com/redhat/demo/ResponseServlet.java
@@ -0,0 +1,58 @@
+/*
+ * #%L
+ * Wildfly Camel
+ * %%
+ * Copyright (C) 2013 - 2015 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.redhat.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@WebServlet(name = "CamelServiceServlet", urlPatterns = { "/registration/*" }, loadOnStartup = 1)
+public class ResponseServlet extends HttpServlet {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private CamelContext camelContext;
+
+    @Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String candidatename = req.getParameter("candidatename");
+        String email = req.getParameter("email");
+        String seatname = req.getParameter("seatname");
+        String seatno = req.getParameter("seatno");
+
+        Registration registration = new Registration(candidatename,email,seatname,seatno);
+    	ProducerTemplate producer = camelContext.createProducerTemplate();
+        producer.requestBody("direct:start", registration);
+
+
+		res.sendRedirect("/done.html");
+    }
+}
