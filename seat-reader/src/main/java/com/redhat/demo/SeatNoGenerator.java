1a6b3b1407c8337a3c2a7aa49646cbdc941f5963
developer <gnunn@redhat.com>
2020-04-23 20:13:03 -0700

    Various fixes

--- seat-reader/src/main/java/com/redhat/demo/SeatNoGenerator.java
+++ seat-reader/src/main/java/com/redhat/demo/SeatNoGenerator.java
@@ -1,4 +1,4 @@
-package org.mycompany;
+package com.redhat.demo;
 
 import java.util.Random;
 
@@ -7,28 +7,28 @@ import org.springframework.stereotype.Component;
 @Component("SeatNoGenerator")
 public class SeatNoGenerator {
 
-	
+
 	public String genSeat() {
 		Random rn = new Random();
 		int rowrange = 19;
 		int row =  rn.nextInt(rowrange) + 1;
 		int colrange = 48;
 		int colno =  rn.nextInt(colrange) + 1;
-		
+
 		if(isSeat(row,colno))
 			return row+"_"+colno;
 		else {
 			return genSeat();
 		}
 	}
-	
+
 	private boolean isSeat(int row, int colno) {
 		switch (colno) {
 			case 9:  return false;
-			case 29:  return false;		
+			case 29:  return false;
 			case 40:  return false;
 		}
-		
+
 		if(colno < 1 || colno > 48) {
 			return false;
 		}else if(row == 1 || row == 2 ) {
@@ -40,7 +40,7 @@ public class SeatNoGenerator {
 			if (colno > (39+row))
 				return false;
 		}
-		
+
 		return true;
 	}
 }
