a1dd7581467ae2647624fb70260e4f24d29c8a57
developer <gnunn@redhat.com>
2020-04-29 18:23:31 -0700

    Add parameters for API key

--- seat-ui/app.js
+++ seat-ui/app.js
@@ -5,14 +5,14 @@ const app = express()
 
 app.use(express.static('js'))
 app.set('view engine', 'ejs');
- 
+
 
 app.get('/', function (req, res) {
-		
+
 	 console.log("process.env.HOSTNAME:["+process.env.ROUTE_HOSTNAME+"]");
    //res.sendFile( __dirname + "/" + "index.html" ,{testenv: process.env.HOSTNAME || 'http://localhost:3000'});
-   
-   res.render("index",{routeurl: process.env.ROUTE_HOSTNAME || 'http://localhost:8080'}) 
+
+   res.render("index",{routeurl: process.env.ROUTE_HOSTNAME || 'http://localhost:8080', api_key: process.env.API_KEY || ''})
    })
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
