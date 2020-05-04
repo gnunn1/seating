a657bf439eb3b3296dadb519b79e030f13745ca2
developer <gnunn@redhat.com>
2020-05-04 11:04:43 -0700

    Add logging headers function to kafka

---
+++ registration-command/src/main/java/com/redhat/demo/LoggingHeaders.java
@@ -0,0 +1,29 @@
+package com.redhat.demo;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.spi.HeaderFilterStrategy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingHeaders implements HeaderFilterStrategy {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingHeaders.class);
+
+    public LoggingHeaders() {
+    }
+
+    @Override
+    public boolean applyFilterToCamelHeaders(String headerName, Object headerValue, Exchange exchange) {
+        String value = (headerValue==null)?"null":headerValue.toString();
+        LOGGER.info("Camel Header: " + headerName + ":" + value);
+        return false;
+    }
+
+    @Override
+    public boolean applyFilterToExternalHeaders(String headerName, Object headerValue, Exchange exchange) {
+        String value = (headerValue==null)?"null":headerValue.toString();
+        LOGGER.info("External Header: " + headerName + ":" + value);
+        return false;
+    }
+}
