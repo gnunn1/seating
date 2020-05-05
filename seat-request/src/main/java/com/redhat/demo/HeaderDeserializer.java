ab055af83c4e92c51b18b813936b0d33766d9279
developer <gnunn@redhat.com>
2020-05-05 04:20:17 -0700

    Fix Jaeger tracing

---
+++ seat-request/src/main/java/com/redhat/demo/HeaderDeserializer.java
@@ -0,0 +1,24 @@
+package com.redhat.demo;
+
+import java.nio.charset.StandardCharsets;
+
+import org.apache.camel.component.kafka.serde.KafkaHeaderDeserializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HeaderDeserializer implements KafkaHeaderDeserializer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingHeaders.class);
+
+    @Override
+    public Object deserialize(String key, byte[] value) {
+        LOGGER.info("Deserializer received key: " + key);
+        if ("uber-trace-id".equals(key)) {
+            LOGGER.info("Deserializing uber-trace-id");
+            return new String(value, StandardCharsets.UTF_8);
+        } else {
+            return value;
+        }
+    }
+}
