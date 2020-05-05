74fbd066dc59f7c735bf934c3b457edaf7dce37d
developer <gnunn@redhat.com>
2020-05-05 03:54:56 -0700

    Add logging, update jeager

--- registration-command/src/main/java/com/redhat/demo/HeaderDeserializer.java
+++ registration-command/src/main/java/com/redhat/demo/HeaderDeserializer.java
@@ -4,11 +4,17 @@ import java.nio.charset.StandardCharsets;
 
 import org.apache.camel.component.kafka.serde.KafkaHeaderDeserializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HeaderDeserializer implements KafkaHeaderDeserializer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingHeaders.class);
 
     @Override
     public Object deserialize(String key, byte[] value) {
         if (key == "uber-trace-id") {
+            LOGGER.info("Deserializing uber-trace-id");
             return new String(value, StandardCharsets.UTF_8);
         } else {
             return value;
